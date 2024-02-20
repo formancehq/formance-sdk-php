1a03b2fe56924bb9c3efcbdb4fb323d8d96c12d0
NumaryBot <support@formance.com>
2024-02-20 18:54:02 +0000

    release(sdk): v2.0.0-rc.10

--- src/SDKConfiguration.php
+++ src/SDKConfiguration.php
@@ -16,10 +16,10 @@ class SDKConfiguration
 	public string $serverUrl = '';
 	public int $serverIndex = 0;
 	public string $language = 'php';
-	public string $openapiDocVersion = 'v2.0.0-rc.9';
-	public string $sdkVersion = 'v2.0.0-rc.9';
+	public string $openapiDocVersion = 'v2.0.0-rc.10';
+	public string $sdkVersion = 'v2.0.0-rc.10';
 	public string $genVersion = '2.237.2';
-	public string $userAgent = 'speakeasy-sdk/php v2.0.0-rc.9 2.237.2 v2.0.0-rc.9 formance-sdk-php';
+	public string $userAgent = 'speakeasy-sdk/php v2.0.0-rc.10 2.237.2 v2.0.0-rc.10 formance-sdk-php';
 	
 
 	public function getServerUrl(): string
